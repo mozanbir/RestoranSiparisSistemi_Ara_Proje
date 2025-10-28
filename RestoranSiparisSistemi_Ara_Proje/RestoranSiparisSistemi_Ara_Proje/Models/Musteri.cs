5fbbad20a0f0651530b8cf6451fd57f19965ddfb
Mustafaozan <mustafaozanbirgili@gmail.com>
2025-10-28 16:25:11 +0300

    Adım 1: Temel OOP sınıflarını (yemek,Musteri) oluşturuldu. .gitignore ile geçici dosyalar yok sayıldı.

---
+++ RestoranSiparisSistemi_Ara_Proje/RestoranSiparisSistemi_Ara_Proje/Models/Musteri.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestoranSiparisSistemi_Ara_Proje.Models
+{
+    public class Musteri
+    {
+        //müşteri özellikleir
+        public int Id { get; set; }
+        public string AdSoyad { get; set; }
+
+        public string Telefon { get; set; }
+
+
+
+        public Musteri(int id, string adsoyad, string telefon)
+        {
+            this.Id = id;
+            this.AdSoyad = adsoyad;
+            this.Telefon = telefon;
+
+        }
+
+
+
+
+
+
+    }
+}
---
+++ RestoranSiparisSistemi_Ara_Proje/RestoranSiparisSistemi_Ara_Proje/Models/Yemek.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestoranSiparisSistemi_Ara_Proje.Models
+{
+    public class Yemek
+    {
+        //Özellikler
+        public int Id { get; set; }
+        public string Ad { get; set; }
+        public double Fiyat { get; set; }
+        public int HazirlanmaSuresi { get; set; }
+
+        //yapıcı metotlarımızı oluşturalım
+        public Yemek(int id, string ad, double fiyat, int sure)
+        {
+            this.Id = id;
+            this.Ad = ad;
+            this.Fiyat = fiyat;
+            this.HazirlanmaSuresi = sure;
+
+
+        }
+        //ToString metodunu override ediyoruz.
+
+        public override string ToString()
+        {
+            //:C para birimi formatı için kullanıyoruz.
+            return $"{Ad} - {Fiyat:C}";
+}
+    }
+}
