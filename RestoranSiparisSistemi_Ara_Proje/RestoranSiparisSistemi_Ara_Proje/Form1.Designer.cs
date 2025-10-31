9369cc9cc7463f9ac8b23ab8658d0956388e3e6f
Mustafaozan <mustafaozanbirgili@gmail.com>
2025-10-31 09:32:19 +0300

    3. Adım Form1 oluşturuldu ve oop ile bağlandı. Form Açıldığında menü listesi doluyor ve aktif sipariş nesnesi oluşuyor.

--- RestoranSiparisSistemi_Ara_Proje/RestoranSiparisSistemi_Ara_Proje/Form1.Designer.cs
+++ RestoranSiparisSistemi_Ara_Proje/RestoranSiparisSistemi_Ara_Proje/Form1.Designer.cs
@@ -28,13 +28,130 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lstMenuler = new System.Windows.Forms.ListBox();
+            this.btnYemekEkle = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblToplamTutar = new System.Windows.Forms.Label();
+            this.btnSiparisTamamla = new System.Windows.Forms.Button();
+            this.btnDurumGuncelle = new System.Windows.Forms.Button();
+            this.rtxSiparisOzet = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Location = new System.Drawing.Point(21, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(241, 68);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "  MENÜ(Yemek Seçimi)";
+            // 
+            // lstMenuler
+            // 
+            this.lstMenuler.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.lstMenuler.FormattingEnabled = true;
+            this.lstMenuler.Location = new System.Drawing.Point(25, 143);
+            this.lstMenuler.Name = "lstMenuler";
+            this.lstMenuler.Size = new System.Drawing.Size(223, 147);
+            this.lstMenuler.TabIndex = 1;
+            // 
+            // btnYemekEkle
+            // 
+            this.btnYemekEkle.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnYemekEkle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnYemekEkle.Location = new System.Drawing.Point(25, 324);
+            this.btnYemekEkle.Name = "btnYemekEkle";
+            this.btnYemekEkle.Size = new System.Drawing.Size(223, 70);
+            this.btnYemekEkle.TabIndex = 2;
+            this.btnYemekEkle.Text = "Sepete Ekle";
+            this.btnYemekEkle.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.RosyBrown;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(293, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(182, 55);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Toplam Tutar";
+            // 
+            // lblToplamTutar
+            // 
+            this.lblToplamTutar.BackColor = System.Drawing.Color.RosyBrown;
+            this.lblToplamTutar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblToplamTutar.Location = new System.Drawing.Point(291, 114);
+            this.lblToplamTutar.MaximumSize = new System.Drawing.Size(200, 200);
+            this.lblToplamTutar.Name = "lblToplamTutar";
+            this.lblToplamTutar.Size = new System.Drawing.Size(184, 50);
+            this.lblToplamTutar.TabIndex = 4;
+            this.lblToplamTutar.Text = "0.00 TL";
+            // 
+            // btnSiparisTamamla
+            // 
+            this.btnSiparisTamamla.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnSiparisTamamla.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSiparisTamamla.Location = new System.Drawing.Point(288, 186);
+            this.btnSiparisTamamla.Name = "btnSiparisTamamla";
+            this.btnSiparisTamamla.Size = new System.Drawing.Size(187, 58);
+            this.btnSiparisTamamla.TabIndex = 5;
+            this.btnSiparisTamamla.Text = "Sipariş Oluştur";
+            this.btnSiparisTamamla.UseVisualStyleBackColor = false;
+            // 
+            // btnDurumGuncelle
+            // 
+            this.btnDurumGuncelle.BackColor = System.Drawing.Color.Teal;
+            this.btnDurumGuncelle.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnDurumGuncelle.Location = new System.Drawing.Point(288, 270);
+            this.btnDurumGuncelle.Name = "btnDurumGuncelle";
+            this.btnDurumGuncelle.Size = new System.Drawing.Size(187, 136);
+            this.btnDurumGuncelle.TabIndex = 6;
+            this.btnDurumGuncelle.Text = "Durumu \'Yolda\'";
+            this.btnDurumGuncelle.UseVisualStyleBackColor = false;
+            // 
+            // rtxSiparisOzet
+            // 
+            this.rtxSiparisOzet.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.rtxSiparisOzet.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rtxSiparisOzet.Location = new System.Drawing.Point(513, 37);
+            this.rtxSiparisOzet.Name = "rtxSiparisOzet";
+            this.rtxSiparisOzet.Size = new System.Drawing.Size(257, 369);
+            this.rtxSiparisOzet.TabIndex = 7;
+            this.rtxSiparisOzet.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(797, 473);
+            this.Controls.Add(this.rtxSiparisOzet);
+            this.Controls.Add(this.btnDurumGuncelle);
+            this.Controls.Add(this.btnSiparisTamamla);
+            this.Controls.Add(this.lblToplamTutar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnYemekEkle);
+            this.Controls.Add(this.lstMenuler);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Restoran Sipariş Sistemi Ara Proje";
+            
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lstMenuler;
+        private System.Windows.Forms.Button btnYemekEkle;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblToplamTutar;
+        private System.Windows.Forms.Button btnSiparisTamamla;
+        private System.Windows.Forms.Button btnDurumGuncelle;
+        private System.Windows.Forms.RichTextBox rtxSiparisOzet;
     }
 }
 
--- RestoranSiparisSistemi_Ara_Proje/RestoranSiparisSistemi_Ara_Proje/Form1.cs
+++ RestoranSiparisSistemi_Ara_Proje/RestoranSiparisSistemi_Ara_Proje/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RestoranSiparisSistemi_Ara_Proje.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,45 @@ namespace RestoranSiparisSistemi_Ara_Proje
 {
     public partial class Form1 : Form
     {
+        private List<Yemek> Menumuz = new List<Yemek>(); //menüdeki yemekleri tutan liste olacak.
+
+        private Musteri Musteri1 = new Musteri(1, "Ali VELİ", "0555 555 55 55");
+
+        private Siparis AktifSiparis;
         public Form1()
         {
             InitializeComponent();
+
+
+            MenuHazirla();
+
+            AktifSiparis = new Siparis(1, Musteri1);
+
+            rtxSiparisOzet.Text = $"Hoş Geldiniz, {Musteri1.AdSoyad} . Yeni Sipariş oluşturuldu (ID:{AktifSiparis.Id}).";
+
+
+        }
+
+        private void MenuHazirla()//menü oluştur 
+        {
+            Menumuz.Add(new Yemek(1, "Kuzu Tandır", 1100.0, 45));
+            Menumuz.Add(new Yemek(2, "Karışık Izgara", 1800.0, 30));
+            Menumuz.Add(new Yemek(3, "Adana Kebap", 450.0, 15));
+            Menumuz.Add(new Yemek(4, "Lahmacun", 250.0, 35));
+
+
+            foreach (Yemek yemek in Menumuz)
+            {
+                lstMenuler.Items.Add(yemek);
+            }
+
+
+
+
+
+
+
+
         }
     }
 }
